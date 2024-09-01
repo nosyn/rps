64f7639fd84d06288fd512bbf34f6a1aec5890ad
Son Nguyen <biem97@gmail.com>
2024-09-01 17:48:06 +0700

    Update header

--- paper/src/components/navbar.tsx
+++ paper/src/components/navbar.tsx
@@ -1,12 +1,14 @@
+import { Button } from '@/components/ui/button';
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
 } from '@/components/ui/tooltip';
-import { Home, MicroscopeIcon, Settings } from 'lucide-react';
+import { Home, MicroscopeIcon, PanelLeft, Settings } from 'lucide-react';
 import Link from 'next/link';
 
-export const SideBar = () => {
+export const MainNavbar = () => {
   return (
     <aside className='fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex'>
       <nav className='flex flex-col items-center gap-4 px-2 sm:py-5'>
@@ -45,5 +47,36 @@ export const SideBar = () => {
         </Tooltip>
       </nav>
     </aside>
+  );
+};
+
+export const MobileNavbar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button size='icon' variant='outline' className='sm:hidden'>
+          <PanelLeft className='h-5 w-5' />
+          <span className='sr-only'>Toggle Menu</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side='left' className='sm:max-w-xs'>
+        <nav className='grid gap-6 text-lg font-medium'>
+          <Link
+            href='#'
+            className='group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base'
+          >
+            <MicroscopeIcon className='h-5 w-5 transition-all group-hover:scale-110' />
+            <span className='sr-only'>Paper Inc</span>
+          </Link>
+          <Link
+            href='#'
+            className='flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground'
+          >
+            <Home className='h-5 w-5' />
+            Dashboard
+          </Link>
+        </nav>
+      </SheetContent>
+    </Sheet>
   );
 };
