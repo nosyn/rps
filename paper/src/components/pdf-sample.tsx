6e21dad4e5179039f578846e8b5c431f0aa24e87
Son Nguyen <biem97@gmail.com>
2024-09-02 10:35:15 +0700

    Have a simple page with pdf view and chat component

--- paper/src/components/pdf-sample.tsx
+++ paper/src/components/pdf-sample.tsx
@@ -57,33 +57,27 @@ export default function PDFSample() {
   }
 
   return (
-    <div className='Example'>
-      <header>
-        <h1>react-pdf sample page</h1>
-      </header>
-      <div className='Example__container'>
-        <div className='Example__container__load'>
-          <label htmlFor='file'>Load from file:</label>{' '}
-          <input onChange={onFileChange} type='file' />
-        </div>
-        <div className='max-h-full overflow-auto' ref={setContainerRef}>
-          <Document
-            file={file}
-            onLoadSuccess={onDocumentLoadSuccess}
-            options={options}
-          >
-            <Outline />
-            {Array.from(new Array(numPages), (_el, index) => (
-              <Page
-                key={`page_${index + 1}`}
-                pageNumber={index + 1}
-                width={
-                  containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth
-                }
-              />
-            ))}
-          </Document>
-        </div>
+    <div>
+      <div>
+        <label htmlFor='file'>Load from file:</label>
+        <input onChange={onFileChange} type='file' />
+      </div>
+      <div ref={setContainerRef}>
+        <Document
+          file={file}
+          onLoadSuccess={onDocumentLoadSuccess}
+          options={options}
+        >
+          {Array.from(new Array(numPages), (_el, index) => (
+            <Page
+              key={`page_${index + 1}`}
+              pageNumber={index + 1}
+              width={
+                containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth
+              }
+            />
+          ))}
+        </Document>
       </div>
     </div>
   );
