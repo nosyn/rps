227117a325f1a38c7f1ff0243ef895e37c960493
Son Nguyen <biem97@gmail.com>
2024-09-03 18:42:03 +0700

    Improve AI chat flow

--- paper/src/app/api/chat/route.ts
+++ paper/src/app/api/chat/route.ts
@@ -1,5 +1,11 @@
 import { vectorStore } from '@/lib/vector-store/qdrant';
-import { AIMessage, HumanMessage } from '@langchain/core/messages';
+import {
+  AIMessage,
+  HumanMessage,
+  SystemMessage,
+} from '@langchain/core/messages';
+import { StringOutputParser } from '@langchain/core/output_parsers';
+import { ChatPromptTemplate } from '@langchain/core/prompts';
 import { ChatOpenAI } from '@langchain/openai';
 import { LangChainAdapter, Message } from 'ai';
 import { createStuffDocumentsChain } from 'langchain/chains/combine_documents';
@@ -16,18 +22,35 @@ export async function POST(req: Request) {
 
   const retriever = (await vectorStore()).asRetriever();
 
-  const model = new ChatOpenAI({
+  const llm = new ChatOpenAI({
     model: 'gpt-3.5-turbo-0125',
     temperature: 0,
   });
 
-  const stream = await model.stream(
-    messages.map((message) =>
+  const questionAnsweringPrompt = ChatPromptTemplate.fromMessages([
+    new SystemMessage(
+      'You are an expert in state of the union topics. You are provided multiple context items that are related to the prompt you have to answer. Use the following pieces of context to answer the question at the end.\n\n{context}'
+    ),
+    ...messages.map((message) =>
       message.role == 'user'
         ? new HumanMessage(message.content)
         : new AIMessage(message.content)
-    )
+    ),
+  ]);
+
+  const combineDocsChain = await createStuffDocumentsChain({
+    llm,
+    prompt: questionAnsweringPrompt,
+    outputParser: new StringOutputParser(),
+  });
+
+  const retrievedDocs = await retriever.invoke(
+    messages[messages.length - 1].content
   );
+
+  const stream = await combineDocsChain.stream({
+    context: retrievedDocs,
+  });
 
   return LangChainAdapter.toDataStreamResponse(stream);
 }
