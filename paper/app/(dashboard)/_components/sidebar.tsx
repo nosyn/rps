c17aeb85f5bb2fbcadbe26fbb89e333239bf1585
Son Nguyen <biem97@gmail.com>
2024-10-05 11:11:48 +0700

    Add references table

--- paper/app/(dashboard)/_components/sidebar.tsx
+++ paper/app/(dashboard)/_components/sidebar.tsx
@@ -1,24 +1,13 @@
 import { Button } from '@/components/ui/button';
-import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
-import { Book, Bot, Code2, LifeBuoy, Settings2, SquareCodeIcon, SquareUser, Triangle } from 'lucide-react';
+import { Book, Settings2, SquareCodeIcon, Triangle } from 'lucide-react';
 
-import { SidebarItem, type SidebarItemProp } from './nav-item';
+import { SidebarItem, type SidebarItemProp } from './sidebar-item';
 
 const NavItems: SidebarItemProp[] = [
   {
     name: 'Playground',
     icon: <SquareCodeIcon className="size-5" />,
     href: '/',
-  },
-  {
-    name: 'Models',
-    icon: <Bot className="size-5" />,
-    href: '#',
-  },
-  {
-    name: 'API',
-    icon: <Code2 className="size-5" />,
-    href: '#',
   },
   {
     name: 'References',
@@ -44,28 +33,6 @@ export const SideBar = () => {
         {NavItems.map((navItem) => (
           <SidebarItem {...navItem} key={navItem.href} />
         ))}
-      </nav>
-      <nav className="mt-auto grid gap-1 p-2">
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button variant="ghost" size="icon" className="mt-auto rounded-lg" aria-label="Help">
-              <LifeBuoy className="size-5" />
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent side="right" sideOffset={5}>
-            Help
-          </TooltipContent>
-        </Tooltip>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button variant="ghost" size="icon" className="mt-auto rounded-lg" aria-label="Account">
-              <SquareUser className="size-5" />
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent side="right" sideOffset={5}>
-            Account
-          </TooltipContent>
-        </Tooltip>
       </nav>
     </aside>
   );
